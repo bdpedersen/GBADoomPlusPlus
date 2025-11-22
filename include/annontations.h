070adea7cb8c7f17e4be5c364bbb5957475dfdfd
Brian Pedersen <brian.dam.pedersen@gmail.com>
2025-11-22 15:12:12 +0100

    [WiP] More compiler warning

---
+++ include/annontations.h
@@ -0,0 +1,14 @@
+#ifndef ANNONTATIONS_H
+#define ANNONTATIONS_H
+
+#define UNUSED __attribute__((unused))
+
+#ifndef GBA
+#define MAYBE_UNUSED UNUSED
+#else
+#define MAYBE_UNUSED
+#endif
+
+
+#endif /* ANNONTATIONS_H */
+
