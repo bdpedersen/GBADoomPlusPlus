9f70cc6293550abd06aa4ef2935b77968e5d3d73
Brian Dam Pedersen <bpedersen@gnresound.com>
2026-01-05 16:25:17 +0100

    Preparing for CONSTMEM address translation

--- include/annotations.h
+++ include/annotations.h
@@ -10,11 +10,35 @@
 #endif
 
 #ifdef __chess__
-#define CONSTMEM chess_storage(PMEM)
+#define CONSTMEMAREA chess_storage(PMEM)
+#define CONSTMEM chess_storage(PMEM%2)
 #else
+#define CONSTMEMAREA
 #define CONSTMEM
 #endif
 
+// Slightly ugly hack allowing address translation for constmem access
+template <typename T> 
+struct ConstMemArray {
+public:
+    ConstMemArray(const T CONSTMEMAREA *ptr ) : data(ptr) {
+        static_assert(sizeof(T)==4);
+    }
+
+    // Read-only accessor
+    const T operator[](size_t index) const {
+        
+        uintptr_t ptr = (uintptr_t)&data[index];
+        #ifdef XLATADDR
+        ptr >>= 1;
+        #endif
+        return *((uint32_t CONSTMEMAREA *)ptr);  
+    }
+    
+private:
+    const T CONSTMEMAREA *data;  // must be public for aggregate initialization
+
+};
 
 
 #endif /* annotations.h */
